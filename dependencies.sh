974bcac9e181d936c121075504afe5ec71f60c70
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-10-30 12:48:37 +0800

    [skip ci]

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sudo apt update --fix-missing
-sudo apt install android* bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick
-lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev
-libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync
-schedtool squashfs-tools xsltproc
-zip zlib1g-dev -y
+sudo apt install android* bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
 sudo apt install python2.7 python-pip -y
 sudo apt autoremove
 sudo apt autoclean
