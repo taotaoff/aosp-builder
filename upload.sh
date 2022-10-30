4868979658f746fec494e1b79940d17ada641279
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-10-30 11:44:12 +0800

    [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone copy out/target/product/a10s/*Unofficial* aosp:final -P || rclone copy out/target/product/a10s/*Alpha*.zip aosp:final -P || rclone copy out/target/product/a10s/*UNOFFICIAL*.zip aosp:final -P || rclone copy out/target/product/a10s/*OFFICIAL*.zip aosp:final -P
+rclone copy out/target/product/a10s/*Unofficial* aosp:final -P || rclone copy out/target/product/a10s/*a10s*.zip aosp:final -P || rclone copy out/target/product/a10s/*UNOFFICIAL*.zip aosp:final -P || rclone copy out/target/product/a10s/*OFFICIAL*.zip aosp:final -P
