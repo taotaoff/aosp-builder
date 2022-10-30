109a85da0f7cc6bda4de69e8af6b335528a6dcc7
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-10-30 11:46:03 +0800

    [skip ci]

--- dependencies.sh
+++ dependencies.sh
@@ -5,6 +5,7 @@ lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurse
 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync
 schedtool squashfs-tools xsltproc
 zip zlib1g-dev -y
+sudo apt install python2.7 python-pip -y
 sudo apt autoremove
 sudo apt autoclean
 sudo apt clean
