6985cd737169d51234badb3f319c7e113c0986b6
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-11-13 07:36:50 +0800

    FlokoROM suzuhime shared system inage

--- gsi.sh
+++ gsi.sh
@@ -15,27 +15,22 @@ BUILD_START=$(date +"%s");
 
 ls -a
 tg_sendText "Cloning GSI Builds"
-git clone https://github.com/MizuNotCool/treble_build_ancient.git
-mkdir lineage-17.x-build-gsi; cd lineage-17.x-build-gsi
-repo init -u https://github.com/Ancient-Lab/manifest.git -b ten-weeabo
-git clone https://github.com/MizuNotCool/treble_build_ancient.git
-git clone https://github.com/Ankits-lab/treble_patches -b lineage-17.1
-git clone https://github.com/Ankits-lab/treble_build_los -b lineage-17.1
+git clone https://github.com/AndyCGYan/treble_experimentations
+mkdir floko-gsi; cd floko-gsi
+git clone https://github.com/AndyCGYan/treble_experimentations
+repo init -u https://github.com/FlokoROM/manifesto.git -b 11.0
+git clone https://github.com/ProjectSuzu/treble_build_floko -b 11.0-unified
+git clone https://github.com/FlokoROM-GSI/lineage_patches_unified -b 11.0-unified
 tg_sendText "Prepairing to build GSI"
 tg_sendText "Building..."
-export KBUILD_BUILD_USER=ItzKaguya
-export KBUILD_BUILD_HOST=ItzKaguya-PC
-export BUILD_USERNAME=ItzKaguya
-export BUILD_HOSTNAME=ItzKaguya-PC
-bash treble_build_ancient/buildbot_treble.sh
+bash treble_build_floko/buildbot_unified.sh treble A64B
 tg_sendText "Build completed! Uploading rom"
-curl bashupload.com -T ancient-18.1-ItzKaguyaGSI-UNOFFICIAL-suzuhimeSharedSystem.img.xz | tee build-output-ancient.txt
-curl bashupload.com -T ./build-output/ancient-18.1-ItzKaguyaGSI-UNOFFICIAL-suzuhimeSharedSystem.img.xz | tee download-link-ancient.txt
+curl bashupload.com -T ./build-output/Floko*.img | tee download-link-floko.txt
 sleep 10
-cat build-output-ancient.txt
-cat download-link-ancient.txt
-tg_sendFile "build-output-ancient.txt"
-tg_sendFile "download-link-ancient.txt"
+curl bashupload.com -T ./build-output/*lineage*.img | tee download-link-lineage.txt
+sleep 10
+tg_sendFile "download-link-floko.txt"
+tg_sendFile "download-link-lineage.txt"
 
 BUILD_END=$(date +"%s");
 DIFF=$(($BUILD_END - $BUILD_START));
