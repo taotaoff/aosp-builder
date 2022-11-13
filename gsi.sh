a3ce7a586acfc16e116e0e2c322e193ee12cb2bf
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-11-13 08:15:51 +0800

    PixelExperience GSI

--- gsi.sh
+++ gsi.sh
@@ -15,22 +15,18 @@ BUILD_START=$(date +"%s");
 
 ls -a
 tg_sendText "Cloning GSI Builds"
-git clone https://github.com/AndyCGYan/treble_experimentations
-mkdir floko-gsi; cd floko-gsi
-git clone https://github.com/AndyCGYan/treble_experimentations
-repo init -u https://github.com/FlokoROM/manifesto.git -b 11.0
-git clone https://github.com/ProjectSuzu/treble_build_floko -b 11.0-unified
-git clone https://github.com/FlokoROM-GSI/lineage_patches_unified -b 11.0-unified
+mkdir pixel; cd pixel
+git clone https://github.com/MizuNotCool/treble_build_pe.git -b twelve --depth=1
 tg_sendText "Prepairing to build GSI"
 tg_sendText "Building..."
-bash treble_build_floko/buildbot_unified.sh treble A64B
+BD=/tmp/itzkaguya/builds
+export BD=/tmp/itzkaguya/builds
+bash treble_build_pe/build.sh twelve
 tg_sendText "Build completed! Uploading rom"
-curl bashupload.com -T ./build-output/Floko*.img | tee download-link-floko.txt
+$BD/PixelExperience-Plus_a64-ab-12.1-ItzKaguyaGSI-UNOFFICIAL.img.xz | tee download-link-pe.txt
 sleep 10
-curl bashupload.com -T ./build-output/*lineage*.img | tee download-link-lineage.txt
-sleep 10
-tg_sendFile "download-link-floko.txt"
-tg_sendFile "download-link-lineage.txt"
+cat download-link-pe
+tg_sendFile "download-link-pe.txt"
 
 BUILD_END=$(date +"%s");
 DIFF=$(($BUILD_END - $BUILD_START));
