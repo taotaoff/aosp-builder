95a6f8c814080e6398f82b3c6dcea2aa2dc27ad9
Raka Prasetya Pratama <106611054+shrxhere@users.noreply.github.com>
2023-01-22 18:56:35 +0800

    another fixes

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone copy out/target/product/mi439/*Unofficial* suzu:test -P || rclone copy out/target/product/mi439/*mi439*.zip suzu:test -P || rclone copy out/target/product/mi439/*UNOFFICIAL*.zip suzu:test -P || rclone copy out/target/product/mi439/*OFFICIAL*.zip suzu:test -P
+rclone copy out/target/product/mi439/*Unofficial* suzu:segawa-builds -P || rclone copy out/target/product/mi439/*mi439*.zip suzu:segawa-builds -P || rclone copy out/target/product/mi439/*UNOFFICIAL*.zip suzu:segawa-builds -P || rclone copy out/target/product/mi439/*OFFICIAL*.zip suzu:segawa-builds -P
