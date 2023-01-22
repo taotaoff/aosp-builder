470d5942956fc2222c86ffa981595aed3c2e3fc9
Raka Prasetya Pratama <106611054+shrxhere@users.noreply.github.com>
2023-01-22 17:13:19 +0800

    Building ShirayukiProject Tsushima 13 for Xiaomi Mi439

--- build.sh
+++ build.sh
@@ -28,11 +28,11 @@ ssh-add ~/.ssh/id_rsa
 echo "$known_hosts" > ~/.ssh/known_hosts
 echo "$user_credentials" > ~/.git-credentials && git config --global credential.helper store
 
-tg_sendText "Syncing rom"
+tg_sendText "Syncing rom (ShirayukiProject Tsushima 13)"
 mkdir -p /tmp/rom
 cd /tmp/rom
 repo init --no-repo-verify --depth=1 -u https://github.com/shirayuki-prjkt/yuki_manifest.git -b tsushima-13 -g default,-device,-mips,-darwin,-notdefault
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) || repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j6 || repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 
 tg_sendText "Downloading trees (Xiaomi Mi439)"
 git clone https://github.com/ShirayukiProject-Devices/android_device_xiaomi_mi439 device/xiaomi/mi439
@@ -48,33 +48,40 @@ lunch shirayuki_mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-export KBUILD_BUILD_USER=ItzKaguya
-export KBUILD_BUILD_HOST=ItzKaguya-PC
-export BUILD_USERNAME=ItzKaguya
-export BUILD_HOSTNAME=ItzKaguya-PC
-export WITH_SU=false
-export WITH_GMS=false
-export TZ=Asia/Makassar
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
+export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
+export BUILD_USERNAME=segawa
+export BUILD_HOSTNAME=itzkaguya-server
+export KBUILD_BUILD_NAME=segawa
+export KBUILD_BUILD_HOST=itzkaguya-server
+export BUILD_BROKEN_CLANG_ASFLAGS=true
+export BUILD_BROKEN_CLANG_CFLAGS=true
 export USE_CCACHE=1
+export CCACHE_COMPRESS=1
+export TZ=Asia/Makassar
 ccache -M 20G
 ccache -o compression=true
 ccache -z
 
 tg_sendText "Starting Compilation.."
 
-mka shirayuki -j$(nproc --all) | tee build.txt
+mka shirayuki -j10 | tee build.txt
 
-tg_sendText "Build completed! Uploading rom"
-curl -T ./out/target/product/mi439/*UNOFFICIAL*.zip temp.sh | tee download-link.txt
+tg_sendText "Build completed! Uploading rom to gdrive"
+rclone copy out/target/product/mi439/*Unofficial* suzu:segawa-builds -P || rclone copy out/target/product/mi439/*Alpha*.zip suzu:segawa-builds -P || rclone copy out/target/product/mi439/Shirayuki*.zip suzu:segawa-builds -P
 
-(ccache -s && echo " " && free -h && echo " " && df -h && echo " " && ls -a out/target/product/a10s/) | tee final_monitor.txt
+(ccache -s && echo " " && free -h && echo " " && df -h && echo " " && ls -a out/target/product/a10/) | tee final_monitor.txt
 tg_sendFile "final_monitor.txt"
 tg_sendFile "build.txt"
-tg_sendFile "download-link.txt"
+
+#tg_sendText "Uploading new ccache to gdrive"
+#cd /tmp
+#tar --use-compress-program="pigz -k -1 " -cf corvus_ccache.tar.gz ccache
+#rclone copy corvus_ccache.tar.gz aosp: -P
 
 BUILD_END=$(date +"%s");
 DIFF=$(($BUILD_END - $BUILD_START));
