7b76db85db892954efefe2a452ba06a131b6159f
Raka Prasetya Pratama <106611054+shrxhere@users.noreply.github.com>
2023-01-22 16:43:07 +0800

    [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rclone copy out/target/product/a10s/*Unofficial* suzu:test -P || rclone copy out/target/product/a10s/*a10s*.zip suzu:test -P || rclone copy out/target/product/a10s/*UNOFFICIAL*.zip suzu:test -P || rclone copy out/target/product/a10s/*OFFICIAL*.zip suzu:test -P
+rclone copy out/target/product/mi439/*Unofficial* suzu:test -P || rclone copy out/target/product/mi439/*mi439*.zip suzu:test -P || rclone copy out/target/product/mi439/*UNOFFICIAL*.zip suzu:test -P || rclone copy out/target/product/mi439/*OFFICIAL*.zip suzu:test -P
